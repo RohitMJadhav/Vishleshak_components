d94d67d8c9ded60dbcb9c23b6bec66d4005b0f2c
Rohit jadhav <rohit.jadhav@neuronimbus.com>
2025-05-14 10:15:03 +0530

    made changes in vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-// import { defineConfig } from 'vite'
-// import tailwindcss from '@tailwindcss/vite';
-// import react from '@vitejs/plugin-react';
-// export default defineConfig({
-//   plugins: [
-//     react(), tailwindcss(),
-//   ],
-// })
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
@@ -26,7 +7,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: 'src/index.ts',
-      name: 'VishleshakComponents',
+      name: 'VibeComponents',
       // fileName: 'vishleshak_components',
       formats: ['es', 'umd'],
     },
